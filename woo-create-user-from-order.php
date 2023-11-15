e83a42e44c2cd243ed3696f83b92ba112f67f255
Alessandro Alessio <info@a2area.it>
2023-11-15 11:47:28 +0100

    add: send email with PHPMailer

--- woo-create-user-from-order.php
+++ woo-create-user-from-order.php
@@ -9,7 +9,14 @@ Author URI: https://www.a2area.it
 Text Domain: a2woo_cuford
 Domain Path: /lang
 */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 require_once 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Invalid request.' );
@@ -29,7 +36,9 @@ function generateRandomPassword($lunghezza = 12) {
 }
 
 add_action('woocommerce_after_register_post_type',  function() {
-    
+
+    $mail = new PHPMailer(true);
+
     // Get last 10 order
     $query = new WC_Order_Query( array(
         'limit' => 50,
@@ -57,6 +66,40 @@ add_action('woocommerce_after_register_post_type',  function() {
         // Join Order with User
         update_post_meta( $order->get_id(), '_customer_user', $user_id );
 
-        echo '<pre style="background: #1f1f1f; padding:1em; border:1px solid #ddd;font-size: 10px;color: #1ed44e;">'; var_dump($user_id); echo '</pre>';
+        die(); 
+
+        // Send Email
+        if ( $_ENV['SEND_WITH_BREVO']!='' && $_ENV['BREVO_USER']!='' && $_ENV['BREVO_PASS']!='' ) :
+            try {
+                //Server settings
+                // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                $mail->isSMTP();
+                $mail->Host       = $_ENV['BREVO_SMTP'];
+                $mail->SMTPAuth   = true;
+                $mail->Username   = $_ENV['BREVO_USER'];
+                $mail->Password   = $_ENV['BREVO_PASS'];
+                // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                $mail->Port       = $_ENV['BREVO_PORT'];
+
+                //Recipients
+                $blogname = get_option('blogname');
+
+                $mail->setFrom($_ENV['BREVO_USER'], $blogname);
+                $mail->addAddress($email);
+
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Here is the subject';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+            die();
+            echo '<pre style="background: #1f1f1f; padding:1em; border:1px solid #ddd;font-size: 10px;color: #1ed44e;">'; var_dump($user_id); echo '</pre>';
+        endif;
     }
 });
